9cd6844f8a31aa2923f1cf5c9f108a4aaf55a138
waterbeside <waterbeside@163.com>
2021-10-14 15:48:43 +0800

    opt: 改进opt.input方法

--- lib/extend/context.ts
+++ lib/extend/context.ts
@@ -1,7 +1,8 @@
 import { Context, Next } from 'koa'
 
+type Code = number | [number, number]
 
-type TApiReturn = (code: number, message: any, data?: any,  extra?: any) => void
+type TApiReturn = (code: Code, message: any, data?: any,  extra?: any) => void
 
 interface IRturnJson {
   code: number
@@ -39,13 +40,20 @@ const setApiReturn = async function(ctx: Context, next: Next): Promise<void> {
    * @param  any $data    [主要数据]
    * @param  any  $extra   [其它]
    */
-  ctx.apiReturn =  function (code: number, message: any = '', data: any = null, extra: any = null) {
+  ctx.apiReturn =  function (code: Code, message: any = '', data: any = null, extra: any = null) {
     if (typeof message === 'object') {
       data = message
       message = null
     }
     data = data || null
     extra = extra || null
+    if ( code instanceof Array ) {
+      if (code.length > 1) {
+        const statusCode = code[1]
+        ctx.status = statusCode
+      }
+      code = code[0]
+    }
     const jsonData: IRturnJson = {
       code,
       msg: message,
@@ -60,18 +68,36 @@ const setApiReturn = async function(ctx: Context, next: Next): Promise<void> {
 
 
 const setInput = async function(ctx: Context, next: Next): Promise<void> {
-
-  ctx.input = function(key: string, method: 'body'|'get' = 'get', option: IInputOption = {}) {
+  /**
+   * 取得请求参数
+   * @param key 请求的参数名，不指定则取全部参数
+   * @param method 请求方法，get|post|body post为body的别称, 不指定时为全取后合并，并且body优先
+   * @param option 请置项 {multi: 参数是否为多个， toNumber:是否转为number类型}
+   * @returns {any}
+   */
+  ctx.input = function(this: any, key: string | null = null, method: 'post'|'get'|'body'| null = null, option: IInputOption = {}) {
     const defaultOption = {
       multi: false,
       toNumber: false,
     }
     const opt:IInputOption = {...defaultOption, ...option}
+
+    // 如果不指定key，则取全部数据 (如果不指定method，则合并get和body的数据)
+    if (key === null) {
+      return {
+        ...(method === null || method === 'get' ? ctx.request.query : {}),
+        ...(method === null || ['post','body'].includes(method) ? ctx.request.body : {})
+      }
+    }
+
+    if (method === null) {
+      return ctx.input(key, 'body', option) || ctx.input(key, 'get', option) 
+    }
     let val = null
     if (method === 'get') {
       val = ctx.request.query[key] || null
     }
-    if (method === 'body') {
+    if (['post','body'].includes(method)) {
       val = (ctx.request.body as any)[key] || null
     }
     if (!val) {
