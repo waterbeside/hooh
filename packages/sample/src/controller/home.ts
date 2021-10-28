f1d6866bf11ed68497ca067ea5be826bac527ffe
waterbeside <waterbeside@163.com>
2021-10-28 23:30:12 +0800

    Example using Nunjucks

--- packages/sample/src/controller/home.ts
+++ packages/sample/src/controller/home.ts
@@ -1,7 +1,7 @@
 import { Controller } from 'hooh'
 class Home extends Controller {
   async index(): Promise<any> {
-    this.ctx.apiReturn(0, {message: 'Welcome to use Hooh'})
+    return this.ctx.render('home/index', {name: 'HOOH'})
   }
 }
 
