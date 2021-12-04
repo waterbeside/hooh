176e585c7203aeaa49672ed537c8324c3b92fbde
waterbeside <waterbeside@163.com>
2021-12-04 15:09:44 +0800

    feat: 路由的控制器名不区分大小写

--- packages/hooh/lib/loader/controller.ts
+++ packages/hooh/lib/loader/controller.ts
@@ -14,18 +14,20 @@ export const loadController = function(controllerPath: string): IControllerLoade
   const files = glob.sync(`${controllerPath}/**/*.${isTsRun ? 'ts' : 'js'}`)
   files.forEach(filepath => {
     filepath = path.resolve(filepath)
-    let pathClassName = filepath.replace(controllerPath, '').toLowerCase()
+    let pathClassName = filepath.replace(controllerPath, '')
+    // console.log('pathClassName', pathClassName)
     pathClassName = pathClassName.substring(1, pathClassName.length - 3)
     pathClassName = pathClassName.replace(/[/|\\]/g, '.')
 
     if (controllerMap.has(pathClassName)) {
       throw new Error(`${controllerPath}文件夹下有${pathClassName}文件同名!`)
     } else {
-      controllerMap.set(pathClassName, filepath)
+      controllerMap.set(pathClassName.toLowerCase(), filepath)
     }
   })
   return {
     getClass(pathClassName: string): any {
+      pathClassName = pathClassName.toLowerCase()
       if (controllerMap.get(pathClassName)) {
         if (!controllerClass.get(pathClassName)) {
           const c = require(controllerMap.get(pathClassName))
