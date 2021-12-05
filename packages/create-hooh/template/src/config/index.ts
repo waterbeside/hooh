ee0901476f0d174942a46fa8f32c26503e88e4ab
waterbeside <waterbeside@163.com>
2021-12-05 12:37:43 +0800

    update

--- packages/create-hooh/template/src/config/index.ts
+++ packages/create-hooh/template/src/config/index.ts
@@ -4,6 +4,8 @@
 
 import redis from './redis'
 import ormconfig from './orm'
+import schedules from './schedules'
+
 
 export default {
     // ormconfig, 
@@ -11,4 +13,5 @@ export default {
     // port: 8080, // The program will start on this port, You can also set it in the arguments to the hooh.start() method or in the HOOH_APP_PORT variable of the .env file. Priority: hooh.start() params > config file > .env file
     lockedErrorCode: -1, // Error code returned when locked, valid after Redis is enabled
     useAutoRouter: true,  // Whether to use automatic routing
+    schedules // cron schedules setting
 }
