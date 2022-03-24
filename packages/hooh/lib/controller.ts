f9610e8743242f8acee72c539de18a08639abcf0
Natron Kan <waterbeside@163.com>
2022-03-24 22:30:04 +0800

    feat: 控制器的apiReturn添加extra参数

--- packages/hooh/lib/controller.ts
+++ packages/hooh/lib/controller.ts
@@ -3,8 +3,8 @@ import BaseLogic, {ILogicOption, ILogicRes} from './logic'
 export {ILogicOption, ILogicRes}
 
 class Controller extends BaseLogic {
-  apiReturn(code: number, msg = '', data: any = null,) {
-    return this.ctx.apiReturn(code, msg, data)
+  apiReturn(code: number, msg = '', data: any = null, extra: any = null) {
+    return this.ctx.apiReturn(code, msg, data, extra)
   }
 }
 
